3f16cec60e405fb8b248862fa17d41e77f2d9268
yaacovmisao747 <103446275+yaacovmisao747@users.noreply.github.com>
2023-06-26 16:58:08 +0300

    Headers

--- frontend/src/components/NavbarCompo/NavbarYona.js
+++ frontend/src/components/NavbarCompo/NavbarYona.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { navItems } from './Data.js';
+import { navItems } from './NavData.js';
 import './CSSYona.css';
 
 function NavbarYona() {
@@ -15,23 +15,33 @@ function NavbarYona() {
 
   return (
     <div className="navbar-container">
-      {navItems.map((item) => (
+      {navItems.map((item, index) => (
         <div
           key={item.id}
           className="navbar-item"
           onMouseEnter={() => handleHover(item.id)}
           onMouseLeave={handleMouseLeave}
         >
-          <div className="navbar-heading">{item.name}</div>
+          <div className="navbar-heading">{item.heading}</div>
           {activeItem === item.id && (
             <div className="navbar-list">
-              {item.children &&
-                item.children.map((child) => (
-                  <div key={child.id} className="navbar-list-item">
-                    <span className="navbar-column navbar-name">{child.name }</span>
-                    <span className="navbar-column navbar-name2">{child.name2}</span>
+              <div className="navbar-column1">
+                {item.list1.map((item) => (
+                  <div key={item.id} className="navbar-list-item">
+                    <span className="navbar-column navbar-name">{item.name}</span>
+                    {item.icon && (
+                      <img className="navbar-icon" src={item.icon} alt={item.name} />
+                    )}
                   </div>
                 ))}
+              </div>
+              <div className="navbar-column2">
+                {item.list2.map((item) => (
+                  <div key={item.id} className="navbar-list-item">
+                    <span className="navbar-column navbar-name">{item.name}</span>
+                  </div>
+                ))}
+              </div>
             </div>
           )}
         </div>
@@ -41,50 +51,3 @@ function NavbarYona() {
 }
 
 export default NavbarYona;
-
-
-
-
-// import React, { useState } from 'react';
-// import { navItems }from './Data.js';
-// import './CSSYona.css'
-
-// function NavbarYona() {
-//   const [activeItem, setActiveItem] = useState(null);
-
-//   const handleHover = (itemId) => {
-//     setActiveItem(itemId);
-//   };
-
-//   const handleMouseLeave = () => {
-//     setActiveItem(null);
-//   };
-
-//   return (
-//     <div className="navbar-container">
-//       {navItems.map((item) => (
-//         <div
-//           key={item.id}
-//           className="navbar-item"
-//           onMouseEnter={() => handleHover(item.id)}
-//           onMouseLeave={handleMouseLeave}
-//         >
-//           <div className="navbar-heading">{item.name}</div>
-//           {activeItem === item.id && (
-//             <div className="navbar-list">
-//               {item.children &&
-//                 item.children.map((child) => (
-//                   <div key={child.id} className="navbar-list-item">
-//                     <img src={child.icon} alt={child.name} className="navbar-icon" />
-//                     <span>{child.name || child.name2}</span>
-//                   </div>
-//                 ))}
-//             </div>
-//           )}
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default NavbarYona;
